19ff5c538b19c4139184201ba6a4a79319f74500
Axel Saliniere <axel.saliniere@protonmail.com>
2023-12-03 20:26:45 +0100

    Circle bonus 2

--- diving_into_classes/circle.py
+++ diving_into_classes/circle.py
@@ -1,20 +1,31 @@
 from math import pi
 
 class Circle:
-
-    def __init__(self, radius = 1):
+    def __init__(self, radius=1):
         self.radius = radius
-
+        
     def __repr__(self):
         return f"Circle({self.radius})"
 
     @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, radius):
+        self._radius = radius
+
+    @property
     def diameter(self):
-        return self.radius * 2
+        return self._radius * 2
+
+    @diameter.setter
+    def diameter(self, diameter):
+        self._radius = diameter / 2
 
     @property
     def area(self):
-        return pi * self.radius ** 2
+        return pi * self._radius ** 2
 
 c = Circle(5)
 print(c)
