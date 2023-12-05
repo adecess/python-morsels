38e862c8200526a85994601664dbee04e9693486
Axel Saliniere <axel.saliniere@protonmail.com>
2023-12-05 21:15:09 +0100

    add Point class solution

---
+++ diving_into_classes/point.py
@@ -0,0 +1,34 @@
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __repr__(self):
+        return f"Point(x={self.x}, y={self.y}, z={self.z})"
+
+    def __eq__(self, other):
+        return (self.x, self.y, self.z) == (other.x, other.y, other.z)
+    
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y, self.z - other.z)
+    
+    def __mul__(self, scalar):
+        return Point(self.x * scalar, self.y * scalar, self.z * scalar)
+
+    def __rmul__(self, scalar):
+        return Point(self.x * scalar, self.y * scalar, self.z * scalar)
+
+    def __iter__(self):
+        return iter((self.x, self.y, self.z))
+
+p1 = Point(1, 2, 3)
+p2 = Point(1, 2, 3)
+print(p1)
+print(p1 == p2)
+p2.x = 4
+print(p1 == p2)
+
