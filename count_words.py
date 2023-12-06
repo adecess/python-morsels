8fd26591cdc6b852046a36e83eb807efa6898afa
Axel Saliniere <axel.saliniere@protonmail.com>
2023-12-07 00:09:26 +0100

    count_words complete solution

--- count_words.py
+++ count_words.py
@@ -1,14 +1,15 @@
+import re
+
 def count_words(sentence):
     word_mapping = dict()
-    words = sentence.casefold().split(" ")
+    words = re.findall(r"\b[\w']+\b", sentence.casefold())
     for word in words:
         if word in word_mapping:
             word_mapping[word] += 1
         else:
             word_mapping[word] = 1
-    
-    return word_mapping
-    
+
+    return word_mapping    
 
 print(count_words("welcome to the jungle"))
 print(count_words("oh what a day what a lovely day"))
