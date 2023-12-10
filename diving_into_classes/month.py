b983264878af6824e7a51ff276e4faf8a75f142f
Axel Saliniere <axel.saliniere@protonmail.com>
2023-12-10 12:05:10 +0100

    add Month factory method and instance method

--- diving_into_classes/month.py
+++ diving_into_classes/month.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+import datetime
+import calendar
 
 @dataclass
 class Month:
@@ -18,6 +20,31 @@ class Month:
         if not isinstance(other, Month):
             return NotImplemented
         return (self.year, self.month) <= (other.year, other.month)
+    
+    @property
+    def first_day(self):
+        return datetime.date(self.year, self.month, 1)
+
+    @property
+    def last_day(self):
+        number_of_month_days = calendar.monthrange(self.year, self.month)[1]
+        return datetime.date(self.year, self.month, number_of_month_days)    
+
+    @classmethod
+    def from_date(cls, date):
+        if not isinstance(date, datetime.date):
+            return NotImplemented
+        return cls(date.year, date.month)
+
+    def strftime(self, format):
+        return datetime.date(self.year, self.month, 1).strftime(format)
 
 dec99 = Month(1999, 12)
 print(dec99)
+print(dec99.first_day)
+print(dec99.last_day)
+
+nye2000 = datetime.date(2000, 12, 31)
+dec2000 = Month.from_date(nye2000)
+print(dec2000)
+print(dec2000.strftime('%b %Y'))
