d11b76b126cfa0f3d748e48213e43fd5b9ed3a88
Axel Saliniere <axel.saliniere@protonmail.com>
2023-12-17 23:14:31 +0100

    count_words bonus 2

--- count_words.py
+++ count_words.py
@@ -1,15 +1,12 @@
-import re
+from collections import Counter
 
 def count_words(sentence):
-    word_mapping = dict()
-    words = re.findall(r"\b[\w']+\b", sentence.casefold())
-    for word in words:
-        if word in word_mapping:
-            word_mapping[word] += 1
-        else:
-            word_mapping[word] = 1
-
-    return word_mapping    
+    words = Counter(
+                word.casefold().strip("¿.,!?\"'")
+                for word in sentence.split()
+            )
+    return words
 
 print(count_words("welcome to the jungle"))
 print(count_words("oh what a day what a lovely day"))
+print(count_words("Oh what a day, what a lovely day!"))
